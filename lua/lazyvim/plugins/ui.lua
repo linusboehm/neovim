2b3f6527684c282da2f869abbd967ac4eeedbffd
Linus Boehm <linus.boehm@lonk.com>
2023-05-04 15:58:44 +0000

    lualine changes

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -112,46 +112,121 @@ return {
     event = "VeryLazy",
     opts = function()
       local icons = require("lazyvim.config").icons
-      local section_settings = {
+      local recording = function (is_active)
+        if not is_active then return {} end
+        return {
+          require("noice").api.statusline.mode.get,
+          cond = require("noice").api.statusline.mode.has,
+          color = { fg = "#ff9e64" },
+        }
+      end
+      local git_diff = function (is_active)
+        if not is_active then return {} end
+        return { "diff",
+          symbols = {
+            added = icons.git.added,
+            modified = icons.git.modified,
+            removed = icons.git.removed,
+          },
+          padding = { left = 0, right = 1 },
+          separator = "‖",
+        }
+      end
+      local diagnostic = function (is_active)
+        if not is_active then return {} end
+         return {
+          "diagnostics",
+          symbols = {
+            error = icons.diagnostics.Error,
+            warn = icons.diagnostics.Warn,
+            info = icons.diagnostics.Info,
+            hint = icons.diagnostics.Hint,
+          },
+        }
+      end
+      local section_settings = function (is_active)
+        return {
         lualine_a = { "mode" },
         lualine_b = {
-          { function() return "" end, separator = "", padding = { left = 1, right = 0 }, },
-          { "branch", separator = "‖", },
-          { "diff",
-            symbols = {
-              added = icons.git.added,
-              modified = icons.git.modified,
-              removed = icons.git.removed,
-            },
-            separator = "‖",
-          },
-          {
-            "diagnostics",
-            symbols = {
-              error = icons.diagnostics.Error,
-              warn = icons.diagnostics.Warn,
-              info = icons.diagnostics.Info,
-              hint = icons.diagnostics.Hint,
-            },
-          },
+          -- { function() return "" end, separator = "", padding = { left = 1, right = 0 }, },
+          { "branch", separator = "", icon = "" },
+          git_diff(is_active),
+          diagnostic(is_active),
         },
         lualine_c = {
           {
+            "filetype",
+            separator = "",
+            icon_only = true,
+            padding = { left = 1, right = 0},
+          },
+          {
             "filename",
             path = 1,
-            symbols = { modified = icons.git.modified, readonly = "", unnamed = "" },
-            separator = "‖"
+            -- symbols = { modified = icons.git.modified, readonly = "", unnamed = "" },
+            symbols = { modified = "" },
+            separator = "",
+            padding = { left = 1, right = 0},
+            color = function()
+              if not is_active then return {} end
+              local buf_nr = vim.api.nvim_get_current_buf()
+              if vim.api.nvim_buf_get_option(buf_nr, 'modified') then
+                return { fg = '#ff9e64' }
+              else
+                return { fg = '#73daca'}
+              end
+            end,
+          },
+          {
+            function ()
+              local buf_nr = vim.api.nvim_get_current_buf()
+              if not vim.api.nvim_buf_get_option(buf_nr, 'modified') then return "" end
+              local undo_tree = vim.fn.undotree()
+              local entries = undo_tree.entries
+              local save_last = undo_tree.save_last
+              if #entries == 0 then return "" end
+              local newhead
+              local curhead
+              local save
+              local found_save = false
+              for key, entry in ipairs(entries) do
+                if entry.newhead then newhead = key end
+                if entry.save then save = key end
+                if entry.curhead then curhead = key - 1 end
+                if entry.save == save_last then found_save = true end
+              end
+              if not found_save then return "[?]" end -- last save is in alternate tree branch
+              if save == nil then save = 0 end
+              if newhead == nil then newhead = 0 end
+              local head if curhead then head = curhead else head = newhead end
+              local mods = head - save
+              return "[" .. mods .. "]"
+            end,
+            padding = { left = 0, right = 0},
+            color = function()
+              if not is_active then return {} end
+              local buf_nr = vim.api.nvim_get_current_buf()
+              if vim.api.nvim_buf_get_option(buf_nr, 'modified') then
+                return { fg = '#ff9e64' }
+              else
+                return { fg = '#73daca'}
+              end
+            end,
           },
         },
-        lualine_x = { "searchcount" },
+        lualine_x = {
+          { "searchcount" , separator = "‖" },
+            recording(is_active)
+        },
         lualine_y = {
           { function() return "" end, separator = "" },
-          { "progress", separator = " ", padding = { left = 1, right = 0 } },
+          { "progress", separator = " ", padding = { left = 0, right = 0 } },
           { "location", padding = { left = 0, right = 1 } },
         },
         -- lualine_z = { function() return "⏱ " .. os.date("%R") end, },
         lualine_z = { function() return " " .. os.date("%R") end, },
       }
+      end
 
       -- local function fg(name)
       --   return function()
@@ -163,13 +238,13 @@ return {
 
       return {
         options = {
-          icons_enabled = false,
+          icons_enabled = true,
           theme = "auto",
           disabled_filetypes = { statusline = { "dashboard", "alpha", "toggleterm" } },
           ignore_focus = { "neo-tree", "toggleterm" },
         },
-        sections = section_settings,
-        inactive_sections = section_settings,
+        sections = section_settings(true),
+        inactive_sections = section_settings(false),
         extensions = { "neo-tree", "lazy" },
       }
     end,
@@ -239,16 +314,16 @@ return {
         command_palette = true,
         long_message_to_split = true,
       },
-      -- cmdline = {
-      --   format = {
-      --     cmdline = { icon = ">_" },
-      --     search_down = { icon = "/" },
-      --     search_up = { icon = "?" },
-      --     filter = { icon = "$" },
-      --     lua = { icon = "☾" },
-      --     help = { icon = "?" },
-      --   },
-      -- },
+      cmdline = {
+        format = {
+          -- cmdline = { icon = ">_" },
+          search_down = { icon = "" },
+          search_up = { icon = "" },
+          -- filter = { icon = "$" },
+          -- lua = { icon = "☾" },
+          -- help = { icon = "?" },
+        },
+      },
       -- format = {
       --   level = {
       --     icons = {
@@ -265,6 +340,9 @@ return {
         -- { filter = { event = "msg_show", find = "E486: Pattern not found:" }, stop = true },
         { filter = { event = "msg_show", find = "E486: Pattern not found:" }, view = 'mini' },
         { filter = { event = "msg_show", kind = "search_count", }, skip = true },
+        { filter = { event = "msg_show", kind = "", find = "written", }, opts = { skip = true }, },
+        { filter = { event = "msg_show", kind = "vim.fn.undotree", }, view = "messages", },
+        -- { filter = { event = "msg_showmode" }, view = "cmdline", },
       }
     },
     -- stylua: ignore
@@ -303,10 +381,10 @@ return {
  ]]
       dashboard.section.header.val = vim.split(logo, "\n")
       dashboard.section.buttons.val = {
-        dashboard.button("f", "⧃ " .. " Find file", ":Telescope find_files <CR>"),
+        dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
         dashboard.button("n", "✚ " .. " New file", ":ene <BAR> startinsert <CR>"),
         dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
-        dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
+        dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
         dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
         dashboard.button("s", " " .. " Restore Session", [[:lua require("persistence").load() <cr>]]),
         dashboard.button("q", " " .. " Quit", ":qa<CR>"),
