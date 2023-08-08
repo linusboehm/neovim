57033f3363a26c519a5d98008cdf9898ab08d493
Linus Boehm <linusboehm@gmail.com>
2023-08-07 22:48:20 -0400

    print write as mini message

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -341,6 +341,7 @@ return {
         { filter = { event = "notify", find = "# Config Change Detected" }, skip = true },
         -- { filter = { event = "msg_show", find = "E486: Pattern not found:" }, stop = true },
         { filter = { event = "msg_show", find = "E486: Pattern not found:" }, view = 'mini' },
+        { filter = { event = "msg_show", find = vim.fn.expand('%') }, view = 'mini' },
         { filter = { event = "msg_show", kind = "search_count", }, skip = true },
         { filter = { event = "msg_show", kind = "", find = "written", }, opts = { skip = true }, },
         { filter = { event = "msg_show", kind = "vim.fn.undotree", }, view = "messages", },
